41bfbf92c44c7ed2e3b9ae3efefbd47d0e29fb4d
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-12-11 13:13:36 +0300

    add task 4

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,19 @@
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 import { APIError, errorResponse } from '../middlewares/error';
+import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 
 const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
 
   api.post('/users', UsersController.postNew);
+  api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
+
+  api.get('/connect', basicAuthenticate, AuthController.getConnect);
+  api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
 
   api.all('*', (req, res, next) => {
     errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
