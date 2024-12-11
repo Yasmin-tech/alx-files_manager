6ce1cc5e537c19746b014c885f62d7b7b7955a2f
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-12-11 13:05:06 +0300

    add task 3

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 import { APIError, errorResponse } from '../middlewares/error';
 
 const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
+
+  api.post('/users', UsersController.postNew);
 
   api.all('*', (req, res, next) => {
     errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
